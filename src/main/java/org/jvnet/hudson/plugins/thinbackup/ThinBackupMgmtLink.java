29773f6d7cea3c9802408eb1eac0b6a860d8bf06
alienllama <alienllama@gmail.com>
2011-01-21 06:42:52 +0000

    o added license headers
    git-svn-id: https://svn.jenkins-ci.org/trunk/hudson/plugins/thinBackup@38272 71c3de6d-444a-0410-be80-ed276b4c234a

--- src/main/java/org/jvnet/hudson/plugins/thinbackup/ThinBackupMgmtLink.java
+++ src/main/java/org/jvnet/hudson/plugins/thinbackup/ThinBackupMgmtLink.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011, Borland (a Micro Focus Company), Matthias Steinkogler, Thomas Fuerer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package org.jvnet.hudson.plugins.thinbackup;
 
 import hudson.Extension;
@@ -21,6 +45,12 @@ import org.kohsuke.stapler.StaplerResponse;
 
 import antlr.ANTLRException;
 
+/**
+ * A backup solution for Hudson. Backs up configuration files from Hudson and its jobs.
+ * 
+ * Originally based on the Backup plugin by Vincent Sellier, Manufacture Française des Pneumatiques Michelin, Romain
+ * Seguy, et.al. Subsequently heavily modified.
+ */
 @Extension
 public class ThinBackupMgmtLink extends ManagementLink {
   private static final Logger LOGGER = Logger.getLogger("hudson.plugins.thinbackup");
