a2fbd757a5c35904f358665c49a055e6dd10008e
Stefan Spieker <S.Spieker@gmx.net>
2022-12-07 17:32:36 +0100

    removed unneeded suppressions

--- src/main/java/org/jvnet/hudson/plugins/thinbackup/utils/EnvironmentVariableNotDefinedException.java
+++ src/main/java/org/jvnet/hudson/plugins/thinbackup/utils/EnvironmentVariableNotDefinedException.java
@@ -1,6 +1,5 @@
 package org.jvnet.hudson.plugins.thinbackup.utils;
 
-@SuppressWarnings("serial")
 public class EnvironmentVariableNotDefinedException extends IllegalArgumentException {
 
   public EnvironmentVariableNotDefinedException() {
